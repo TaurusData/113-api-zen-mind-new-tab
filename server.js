4342dab4b4fae301d26f2e511d1f3317ee9f5b88
Petar Petrov <petarpetrov@Petars-MacBook-Pro.local>
2019-11-05 17:58:03 +0200

    fix: add process env

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.static('assets'))
+app.use(express.static('assets'));
 
-app.listen(3000, function() {
+app.get('/', function (req, res) {
+  res.send(JSON.stringify({ Hello: 'World'}));
+ });
+
+app.listen(port, function() {
   console.log("Listening on port 3000!");
 });
